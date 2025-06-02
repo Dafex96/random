c237fa8375ca88f8c962d8a3dc15e9fdc1fba548
David <davicolo717@gmail.com>
2025-06-02 19:53:50 -0400

    cambios

--- Drafts/draft.py
+++ Drafts/draft.py
@@ -1,2 +1,19 @@
-for i in range(1,11):
-    print("El cris e weco")
+equipos = ["Colo-Colo","Universidad de Chile","Universidad Catolica","Magallanes","Santiago Wanderers"]
+
+equipos.append("Union Española")
+####### Agrega al final de la lista
+
+equipos.insert(1, "Huachipato")
+####### Agrega en una poscicion especifica
+
+equipos.remove("Universidad de Chile")
+####### Elimina un elemento en especifico
+
+equipos.reverse()
+####### Invierte el orden de la lista
+
+for i in equipos:
+    print(i)
+
+
+
